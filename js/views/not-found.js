dc513f9e2a5be1093dc81366d9351a15db2dc1cf
Uknowner <154834844+Uknowner@users.noreply.github.com>
2026-05-26 08:57:16 +0200

    Upload js/views/not-found.js

---
+++ js/views/not-found.js
@@ -0,0 +1,10 @@
+export function render() {
+    return `
+    <div class="cards">
+        <div class="card text-center">
+            <h2>Page Not Found</h2>
+            <p>The page you're looking for doesn't exist.</p>
+            <a href="#home" class="btn">Go Home</a>
+        </div>
+    </div>`;
+}
